043ec843313de08b41aece8814cf536bbbfd7199
dragobabyarm <102838976+dragobabyarm@users.noreply.github.com>
2023-02-21 18:03:13 -0600

    numbers

---
+++ Numers.py
@@ -0,0 +1,8 @@
+num_char = input("")
+#digit = str(word)
+digit_1 = (num_char[0])
+digit_2 = (num_char[1])
+sum = (int(num_char[0]) + int(num_char[1]))
+
+print(f"{digit_1} + {digit_2} = {sum}")
+print(int(digit_1) + int(digit_2))
